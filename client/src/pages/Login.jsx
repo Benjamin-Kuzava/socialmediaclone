96ccbd8aabc1914c8fc3b22b348b0bcb6432028f
Benjamin-Kuzava <kuzav1bj@gmail.com>
2021-04-22 18:17:20 -0400

    continued work on login page styling

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 
 export default function Login() {
@@ -37,45 +37,50 @@ export default function Login() {
         <img src="/images/iphone-with-profile.jpeg" alt="Iphone with app displayed." />
       </div>
       <div className="flex flex-col w-2/5">
-        <h1 className="flex justify-center w-full">
-          <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
-        </h1>
-        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+          <h1 className="flex justify-center w-full">
+            <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+          </h1>
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-        <form onSubmit={handleLogin} method="POST">
-          <input
-            type="text"
-            aria-label="Enter your email address"
-            placeholder="Email Address"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            name="email"
-            value={email}
-            onChange={handleChange}
-          />
-          <input
-            type="password"
-            aria-label="Enter your password"
-            placeholder="Password"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            name="password"
-            value={password}
-            onChange={handleChange}
-          />
-          <button
-            disabled={isInvalid}
-            type="submit"
-            className={`bg-blue-500 text-white w-full rounded h-8 font-bold
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              type="text"
+              aria-label="Enter your email address"
+              placeholder="Email Address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              name="email"
+              value={email}
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              aria-label="Enter your password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              name="password"
+              value={password}
+              onChange={handleChange}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold
               ${isInvalid && 'opacity-50'}`}
-          >
-            Log In
-          </button>
-        </form>
+            >
+              Log In
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
+          <p className="text-sm">
+            Don't have an account?{` `}
+            <Link to="/signup" className="font-bold text-blue-medium">
+              Sign Up
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
 }
-
-// text-red-primary
-// text-gray-base
-// border-gray-primary
-// bg-blue-medium
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,28 @@
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true
+  },
+  theme: {
+    colors: {
+      white: '#ffffff',
+      blue: {
+        medium: '#005c98'
+      },
+      black: {
+        light: '#005c98',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      }
+    }
   }
 };
+
+// text-red-primary
+// text-gray-base
+// border-gray-primary
+// bg-blue-medium
+// text-blue-medium
